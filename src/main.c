7b20dfd69e5c748e8f35f35b114caeb3eed9f579
Aavan Vostro <alirezainallo@gmail.com>
2024-11-07 12:14:42 +0330

    make it clean

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
  
-int main(void)
-{
+int main(void){
   DDRA = DDRB = DDRC = DDRD = 0xff;
   PORTA = PORTB = PORTC = PORTD = 0x00;
 
-  while (1)
-  {
+  while (1){
     _delay_ms(500);
     PORTA ^= (1 << 1);
     _delay_ms(500);
